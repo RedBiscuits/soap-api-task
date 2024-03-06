53187f8ba8cbdef462d72718a353ddb627365ebd
RedBiscuits <84057046+RedBiscuits@users.noreply.github.com>
2024-03-06 14:27:25 +0200

    Feature: update country via SOAP call

--- app/Http/Services/CountrySoapService.php
+++ app/Http/Services/CountrySoapService.php
@@ -3,6 +3,7 @@
 namespace App\Http\Services;
 
 use App\Models\Country;
+use App\Models\Log;
 use Exception;
 use nusoap_server;
 
@@ -19,7 +20,7 @@ class CountrySoapService
 
         $this->server->register(
             'getCountries',
-            ['callback_url' => 'xsd:string'],
+            [],
             ['countries' => 'xsd:string'],
             'urn:countryService',
             'urn:countryService#getCountries',
@@ -27,15 +28,48 @@ class CountrySoapService
             'encoded',
             'Returns a list of countries'
         );
+
+        $this->server->register(
+            'updateCountry',
+            ['id' => 'xsd:int', 'callback_url' => 'xsd:string', 'name' => 'tns:ArrayOfString', 'description' => 'tns:ArrayOfString'],
+            ['country' => 'tns:ArrayOfString'],
+            'urn:countryService',
+            'urn:countryService#updateCountry',
+            'rpc',
+            'encoded',
+            'Updates a country and returns the updated country object'
+        );
+
     }
 
-    public function getCountries($callback_url = '')
+    public function getCountries()
     {
         // Fetch countries from the database
         $countries = Country::all();
 
         // Return the list of countries in the desired format
         return $countries->toArray();
+    }
+
+    public function updateCountry(int $id , string $callback_url , array $name , array $description ){
+        $country = Country::find($id);
+        $country->fill([
+            'name' => $name,
+            'description' => $description
+        ]);
+
+        Log::create([
+            'old_values' => $country->getDirty(),
+            'new_values' => [
+                'name' => $name,
+                'description' => $description
+            ],
+            'callback_url' => $callback_url
+        ]);
+
+        $country->save();
+
+        return $country;
     }
 
     public function run()
